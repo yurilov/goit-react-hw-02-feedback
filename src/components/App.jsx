717a93588b67a1cec07f370046e92dadba69fdb3
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-05-09 13:17:44 +0300

    changes App to class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,24 @@
+import React, { Component } from "react";
 import './App.css';
 import Feedback from './Feedback/Feedback';
 import Statistics from './Statistics/Statistics';
 
-export const App = () => {
-  return (
-    <div className="App">
-      <Feedback />
-      <Statistics />
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <>
+        <Feedback />
+        <Statistics />     
+      </>
+    );
+  }
+}
+export default App;
